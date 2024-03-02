782b5b98e6f1ef07320ae2efc841e1cb9f2e44b0
Ridoy <ridoy.starlord@gmail.com>
2024-03-02 13:56:54 +0600

    Feat: Solve the problem-1 sorting and sorting issues

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,26 +1,95 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+const initialValue = { name: "", status: "" };
 
 const Problem1 = () => {
   const [show, setShow] = useState("all");
+  const [currentTask, setCurrentTask] = useState(initialValue);
+  const [taskList, setTaskList] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
 
   const handleClick = (val) => {
     setShow(val);
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTaskList((prev) => [...prev, currentTask]);
+    setCurrentTask(initialValue);
+  };
+
+  const sortTasksList = [...filteredList].sort((a, b) => {
+    if (
+      a.status.toLowerCase() === "active" &&
+      b.status.toLowerCase() !== "active"
+    )
+      return -1;
+    if (
+      a.status.toLowerCase() === "completed" &&
+      b.status.toLowerCase() !== "active" &&
+      b.status.toLowerCase() !== "completed"
+    )
+      return -1;
+    if (
+      b.status.toLowerCase() === "active" &&
+      a.status.toLowerCase() !== "active"
+    )
+      return 1;
+    if (
+      b.status.toLowerCase() === "completed" &&
+      a.status.toLowerCase() !== "active" &&
+      a.status.toLowerCase() !== "completed"
+    )
+      return 1;
+    return a.status.toLowerCase().localeCompare(b.status.toLowerCase());
+  });
+
+  useEffect(() => {
+    if (show === "all") {
+      setFilteredList([...taskList]);
+    } else if (show === "active") {
+      setFilteredList(
+        [...taskList].filter((task) => task.status.toLowerCase() === "active")
+      );
+    } else if (show === "completed") {
+      setFilteredList(
+        [...taskList].filter(
+          (task) => task.status.toLowerCase() === "completed"
+        )
+      );
+    } else {
+      setFilteredList([...taskList]);
+    }
+  }, [show, taskList]);
 
   return (
     <div className="container">
       <div className="row justify-content-center mt-5">
         <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
         <div className="col-6 ">
-          <form className="row gy-2 gx-3 align-items-center mb-4">
+          <form
+            className="row gy-2 gx-3 align-items-center mb-4"
+            onSubmit={handleSubmit}
+          >
             <div className="col-auto">
-              <input type="text" className="form-control" placeholder="Name" />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                value={currentTask.name}
+                onChange={(e) =>
+                  setCurrentTask({ ...currentTask, name: e.target.value })
+                }
+              />
             </div>
             <div className="col-auto">
               <input
                 type="text"
                 className="form-control"
                 placeholder="Status"
+                value={currentTask.status}
+                onChange={(e) =>
+                  setCurrentTask({ ...currentTask, status: e.target.value })
+                }
               />
             </div>
             <div className="col-auto">
@@ -68,7 +137,16 @@ const Problem1 = () => {
                 <th scope="col">Status</th>
               </tr>
             </thead>
-            <tbody></tbody>
+            <tbody>
+              {sortTasksList?.map((task, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{task.name}</td>
+                    <td>{task.status}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
           </table>
         </div>
       </div>
